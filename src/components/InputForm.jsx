4ff297d5eb21722f3e4ffcfaf0d75c403f8e788a
yuki1567 <19961017yuki@gmail.com>
2025-02-15 21:48:03 +0900

    add: formのモック作成

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function InputForm() {
+  return (
+    <div class="input-form">
+      <form>
+        <input type="text" />
+        <button>
+          <span class="material-icons add-icon">add_circle</span>
+        </button>
+      </form>
+    </div>
+  );
+}
