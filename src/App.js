754b2e1480ebc24eb32328c165fa03f15fc58358
yuki1567 <19961017yuki@gmail.com>
2025-02-15 21:46:34 +0900

    add: マテリアルアイコンの導入

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import "material-icons/iconfont/material-icons.css";
 import "./App.css";
 import { Title } from "./components/Title";
+import { InputForm } from "./components/InputForm";
+import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
     <div className="body">
       <Title />
+      <InputForm />
+      <TodoList />
     </div>
   );
 }
